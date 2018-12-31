d156da0d837ba39d14df6539aa8f15f4b1faaf7c
Mark Nguyen <marknltv@comcast.net>
2018-12-30 19:54:52 -0800

    Return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i

--- codechallenge_002.py
+++ codechallenge_002.py
@@ -10,7 +10,6 @@ Given an array of integers, return a new array such that each element at index i
 For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
 Follow-up: what if you can't use division?
 
-
 Algorithm: prodArray()
 ======================
 Input: A list of integers
