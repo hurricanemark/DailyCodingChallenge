b9ad184da6b42663b5467895a50fa8df4728bbf0
Mark Nguyen <marknltv@comcast.net>
2019-01-09 12:21:23 -0800

    Given an unordered list of flights taken by someone, each represented as (origin, destination) pairs, and a starting airport, compute the persons itinerary.

--- codechallenge_027.py
+++ codechallenge_027.py
@@ -4,10 +4,21 @@ Date: 01/09/2019
 Problem descriptions:
 ====================
 This problem was asked by Facebook.
-Given an unordered list of flights taken by someone, each represented as (origin, destination) pairs, and a starting airport, compute the person's itinerary. If no such itinerary exists, return null. If there are multiple possible itineraries, return the lexicographically smallest one. All flights must be used in the itinerary.
-For example, given the list of flights [('SFO', 'HKO'), ('YYZ', 'SFO'), ('YUL', 'YYZ'), ('HKO', 'ORD')] and starting airport 'YUL', you should return the list ['YUL', 'YYZ', 'SFO', 'HKO', 'ORD'].
-Given the list of flights [('SFO', 'COM'), ('COM', 'YYZ')] and starting airport 'COM', you should return null.
-Given the list of flights [('A', 'B'), ('A', 'C'), ('B', 'C'), ('C', 'A')] and starting airport 'A', you should return the list ['A', 'B', 'C', 'A', 'C'] even though ['A', 'C', 'A', 'B', 'C'] is also a valid itinerary. However, the first one is lexicographically smaller.
+
+Given an unordered list of flights taken by someone, each represented as 
+(origin, destination) pairs, and a starting airport, compute the person's itinerary. 
+If no such itinerary exists, return null. If there are multiple possible itineraries, 
+return the lexicographically smallest one. All flights must be used in the itinerary.
+
+For example, given the list of flights [('SFO', 'HKO'), ('YYZ', 'SFO'), ('YUL', 'YYZ'), ('HKO', 'ORD')] 
+and starting airport 'YUL', you should return the list ['YUL', 'YYZ', 'SFO', 'HKO', 'ORD'].
+
+Given the list of flights [('SFO', 'COM'), ('COM', 'YYZ')] and starting airport 'COM', 
+you should return null.
+
+Given the list of flights [('A', 'B'), ('A', 'C'), ('B', 'C'), ('C', 'A')] and starting airport 'A', 
+you should return the list ['A', 'B', 'C', 'A', 'C'] even though ['A', 'C', 'A', 'B', 'C'] is 
+also a valid itinerary. However, the first one is lexicographically smaller.
 
 
 Algorithm:
@@ -17,7 +28,8 @@ Output: An array of tuples (representing connecting flights)
 Psuedo code:
 1.  Check for valid input
 2.  Initialize an empty itinerary list. Assign departure points from the flight list
-4.  determine the destination point of the given starting airport.  Append start,end points to the Itnerary list.
+4.  determine the destination point of the given starting airport.  Append start,end points to the 
+itnerary list.
 5.  determine the arrival airport from the above destination, next start point equals to arrival airport.
 6.  keep doing step4,5 until the available flight list is exhausted
 7.  return the itineray list
