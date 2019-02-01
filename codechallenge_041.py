20850aad8f47ba681959e4b7fe1ea453773fb19a
Mark Nguyen <marknltv@comcast.net>
2019-02-01 10:05:20 -0800

    Given a multiset of integers, return whether it can be partitioned into two subsets whose sums are the same.

--- codechallenge_041.py
+++ codechallenge_041.py
@@ -23,64 +23,79 @@ Output: A boolean
 Psuedo code:
 
 1.  Validate input
-2.  Let's convert set to list for easier manipulation
-3.  Generate a function to yield every posible sublists of the given list
-4.  For each subset, calculate sum and store in a list called sums.
-5.  Determine if there are dupplicates in sums, if so return True, else False
+2.  Since set does not allow dupplicates, we will represent multiset using list for simplicity.
+3.  Sort the list.  Iterate list index from both ends to yield every posible sublists of the given list
+4.  For each subset, calculate sum and compare to half sum of the total list.
+5.  Return True if sum of sublist equals half-sum, else False
 
 
 '''
-
-#
-# generator that yields possible combinations of two sub-lists
-# where elements of the combined two sublists should 
-# be that of the original list
-#
-def gen_sublist(L=[]):
-	start, end = 0, len(L)
-	x_end = end
-	sub_list = []
-	while start < end -1:
-		sub_listA = L[start:x_end]
-		x_end -= 1
-		sub_listB = L[x_end+1:end] + L[:start]
-
-		yield sub_listA, sub_listB
-	
-		if x_end < start + 1:
-			start += 1
-			x_end = end
 
 
 #
 # return True if the multiset can be broken up into subsets
 #
-def isSubset_able(S={}):
-	# conver set into list
-	L = list(S)
-	if len(L) == 0:
-		return False
-
-	lists_A_B = gen_sublist(L)
-	for sublist in lists_A_B:
-		print("DBUG-- L1:{} L1_SUM:{} -- L2:{} L2_SUM:{}".format(sublist[0], sum(sublist[0]), sublist[1], sum(sublist[1])))
-		if sum(sublist[0]) == sum(sublist[1]):
-			print("\nYES ==> L1:{} L1_SUM:{} -- L2:{} L2_SUM:{}".format(sublist[0], sum(sublist[0]), sublist[1], sum(sublist[1])))
+def isSub_able(R1, L2, halfTotal):
+	l_total=0
+	r_total=0
+	for r_elem, l_elem in zip(R1, L2):
+		r_total += r_elem
+		l_total += l_elem
+		#print("DBUG-- r_elem:{} l_elem:{}".format(r_elem, l_elem))
+		if l_total == halfTotal or r_total == halfTotal:
+			print("DBUG-- R_Total:{} L_Total:{} Half:{}".format(r_total, l_total, halfTotal))
 			return True
 	return False
 
+#
+# process list by disecting it incrementally
+#
+def get_sublist(L=[]):
+	if len(L) == 0:
+		return False
+
+	halfSum = sum(L)//2
+	L.sort()
+	start, end = 0, len(L)
+	x_end = end
+
+	while start < end -1:
+		sub_listA = L[start:x_end]
+		x_end -= 1
+		sub_listB = L[x_end+1:end] + L[:start]
+
+		if isSub_able(sub_listA, sub_listB, halfSum):
+			return True
+
+		if x_end < start + 1:
+			start += 1
+			x_end = end
+	return False
+
+#
+# unittest
+#
+def test_sublisting():
+	myMSet = [15, 5, 20, 10, 35, 15, 10]
+	assert get_sublist(myMSet) == True
+	myMSet = [15, 5, 20, 10, 35]
+	assert get_sublist(myMSet) == False
 
 #
 # client program
 #
 def main():
-	mySet = {15, 5, 20, 10, 35, 15, 10}
-	print("\nTest1:")
-	print("Can it be broken into subsets?: {}".format(isSubset_able(mySet)))
-	mySet = {15, 5, 20, 10, 35}
-	print("\nTest2:")
-	print("Can it be broken into subsets?: {}".format(isSubset_able(mySet)))
+	myMSet = [15, 5, 20, 10, 35, 15, 10]
+	print("\nTest1:\nGiven multiset {}".format(myMSet))
+	print("Can it be broken into subsets?: {}".format(get_sublist(myMSet)))
 
+	myMSet = [15, 5, 20, 10, 35]
+	print("\nTest2:\nGiven multiset {}".format(myMSet))
+	print("Can it be broken into subsets?: {}".format(get_sublist(myMSet)))
+
+	myMSet = []
+	print("\nTest3:\nGiven multiset {}".format(myMSet))
+	print("Can it be broken into subsets?: {}".format(get_sublist(myMSet)))
 
 if __name__ == '__main__':
 	main()
@@ -89,42 +104,28 @@ if __name__ == '__main__':
 '''
 Run-time output:
 ================
-
 (DailyCodingChallenge-wC3ocw3s) markn@raspberrypi3:~/devel/py-src/DailyCodingChallenge $ python codechallenge_041.py
 
 Test1:
-DBUG-- L1:[35, 5, 10, 15, 20] L1_SUM:85 -- L2:[] L2_SUM:0
-DBUG-- L1:[35, 5, 10, 15] L1_SUM:65 -- L2:[20] L2_SUM:20
-DBUG-- L1:[35, 5, 10] L1_SUM:50 -- L2:[15, 20] L2_SUM:35
-DBUG-- L1:[35, 5] L1_SUM:40 -- L2:[10, 15, 20] L2_SUM:45
-DBUG-- L1:[35] L1_SUM:35 -- L2:[5, 10, 15, 20] L2_SUM:50
-DBUG-- L1:[5, 10, 15, 20] L1_SUM:50 -- L2:[35] L2_SUM:35
-DBUG-- L1:[5, 10, 15] L1_SUM:30 -- L2:[20, 35] L2_SUM:55
-DBUG-- L1:[5, 10] L1_SUM:15 -- L2:[15, 20, 35] L2_SUM:70
-DBUG-- L1:[5] L1_SUM:5 -- L2:[10, 15, 20, 35] L2_SUM:80
-DBUG-- L1:[10, 15, 20] L1_SUM:45 -- L2:[35, 5] L2_SUM:40
-DBUG-- L1:[10, 15] L1_SUM:25 -- L2:[20, 35, 5] L2_SUM:60
-DBUG-- L1:[10] L1_SUM:10 -- L2:[15, 20, 35, 5] L2_SUM:75
-DBUG-- L1:[15, 20] L1_SUM:35 -- L2:[35, 5, 10] L2_SUM:50
-DBUG-- L1:[15] L1_SUM:15 -- L2:[20, 35, 5, 10] L2_SUM:70
-Can it be broken into subsets?: False
+Given multiset [15, 5, 20, 10, 35, 15, 10]
+DBUG-- R_Total:15 L_Total:55 Half:55
+Can it be broken into subsets?: True
 
 Test2:
-DBUG-- L1:[35, 5, 10, 15, 20] L1_SUM:85 -- L2:[] L2_SUM:0
-DBUG-- L1:[35, 5, 10, 15] L1_SUM:65 -- L2:[20] L2_SUM:20
-DBUG-- L1:[35, 5, 10] L1_SUM:50 -- L2:[15, 20] L2_SUM:35
-DBUG-- L1:[35, 5] L1_SUM:40 -- L2:[10, 15, 20] L2_SUM:45
-DBUG-- L1:[35] L1_SUM:35 -- L2:[5, 10, 15, 20] L2_SUM:50
-DBUG-- L1:[5, 10, 15, 20] L1_SUM:50 -- L2:[35] L2_SUM:35
-DBUG-- L1:[5, 10, 15] L1_SUM:30 -- L2:[20, 35] L2_SUM:55
-DBUG-- L1:[5, 10] L1_SUM:15 -- L2:[15, 20, 35] L2_SUM:70
-DBUG-- L1:[5] L1_SUM:5 -- L2:[10, 15, 20, 35] L2_SUM:80
-DBUG-- L1:[10, 15, 20] L1_SUM:45 -- L2:[35, 5] L2_SUM:40
-DBUG-- L1:[10, 15] L1_SUM:25 -- L2:[20, 35, 5] L2_SUM:60
-DBUG-- L1:[10] L1_SUM:10 -- L2:[15, 20, 35, 5] L2_SUM:75
-DBUG-- L1:[15, 20] L1_SUM:35 -- L2:[35, 5, 10] L2_SUM:50
-DBUG-- L1:[15] L1_SUM:15 -- L2:[20, 35, 5, 10] L2_SUM:70
+Given multiset [15, 5, 20, 10, 35]
 Can it be broken into subsets?: False
 
+Test3:
+Given multiset []
+Can it be broken into subsets?: False
+(DailyCodingChallenge-wC3ocw3s) markn@raspberrypi3:~/devel/py-src/DailyCodingChallenge $ pytest codechallenge_041.py
+=================================== test session starts ===================================
+platform linux2 -- Python 2.7.13, pytest-3.6.3, py-1.5.4, pluggy-0.6.0
+rootdir: /home/markn/devel/py-src/DailyCodingChallenge, inifile:
+collected 1 item
+
+codechallenge_041.py .                                                              [100%]
+
+================================ 1 passed in 0.13 seconds =================================
 
 '''
