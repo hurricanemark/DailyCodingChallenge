1ca4e8173a6c6a088f260b6b0109d0b77cf24af6
Mark Nguyen <marknltv@comcast.net>
2019-02-09 16:09:07 -0800

    Given an integer value n, find out the n-th positive integer whose sum is 10.

--- codechallenge_045.py
+++ codechallenge_045.py
@@ -29,48 +29,93 @@ And, all multiples of 9 are present in arithmetic progression 19, 28, 37, 46, 55
 import itertools
 
 def findNthFactor(N):  
-    count = 0; 
-    n_fact = 19; 
-    while (True):  
+	if N <= 0:
+		return -1
+
+	count = 0 
+	n_fact = 19 
+	while (True):  
   
-        # Find sum of digits in 
-        # n_fact no.  
-        sum = 0; 
-        x = n_fact; 
-        while (x > 0): 
-            sum = sum + x % 10; 
-            x = int(x / 10); 
+		# Find sum of digits in 
+		# n_fact no.  
+		sum = 0 
+		x = n_fact 
+		while (x > 0): 
+			sum = sum + x % 10 
+			x = int(x / 10) 
   
-        # If sum is 10, we increment 
-        # count  
-        if (sum == 10):  
-            count+=1;  
+		# If sum is 10, we increment 
+		# count  
+		if (sum == 10):  
+			count+=1  
   
-        # If count becomes N, we return  
-        # n_fact number.  
-        if (count == N):  
-            return n_fact; 
+		# If count becomes N, we return  
+		# n_fact number.  
+		if (count == N):  
+			return n_fact 
           
-        n_fact += 9; 
+		n_fact += 9 
   
-    return -1;
+	return -1
 
 
 def test_ginfNthFator():
-	pass
+	assert findNthFactor(0) == -1
+	assert findNthFactor(1) == 19
+	assert findNthFactor(2) == 28
 
 
 def main():
-	print("\nTest1:"
+	print("\nTest1:")
 	A = [i+1 for i in range(10)]
 	B = [findNthFactor(A[i]) for i in range(len(A))]
 	for i,n in enumerate(B):
 		print("n:{}, perfect n-th:{}".format(i+1,n))	
-	print("\nTest2:"
+	print("\nTest2:")
 	A = [i+1 for i in range(10,20,1)]
 	B = [findNthFactor(A[i]) for i in range(len(A))]
 	for i,n in enumerate(B):
-		print("n:{}, perfect n-th:{}".format(i+1,n))	
+		print("n:{}, perfect n-th:{}".format(i+11,n))	
 
 if __name__ == '__main__':
 	main()
+
+'''
+Run-time output:
+===============
+
+(DailyCodingChallenge-wC3ocw3s) markn@raspberrypi3:~/devel/py-src/DailyCodingChallenge $ python codechallenge_045.py
+
+Test1:
+n:1, perfect n-th:19
+n:2, perfect n-th:28
+n:3, perfect n-th:37
+n:4, perfect n-th:46
+n:5, perfect n-th:55
+n:6, perfect n-th:64
+n:7, perfect n-th:73
+n:8, perfect n-th:82
+n:9, perfect n-th:91
+n:10, perfect n-th:109
+
+Test2:
+n:11, perfect n-th:118
+n:12, perfect n-th:127
+n:13, perfect n-th:136
+n:14, perfect n-th:145
+n:15, perfect n-th:154
+n:16, perfect n-th:163
+n:17, perfect n-th:172
+n:18, perfect n-th:181
+n:19, perfect n-th:190
+n:20, perfect n-th:208
+(DailyCodingChallenge-wC3ocw3s) markn@raspberrypi3:~/devel/py-src/DailyCodingChallenge $ pytest codechallenge_045.py
+==================================== test session starts ====================================
+platform linux2 -- Python 2.7.13, pytest-3.6.3, py-1.5.4, pluggy-0.6.0
+rootdir: /home/markn/devel/py-src/DailyCodingChallenge, inifile:
+collected 1 item
+
+codechallenge_045.py .                                                                [100%]
+
+================================= 1 passed in 0.06 seconds ==================================
+'''
