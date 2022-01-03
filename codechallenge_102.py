9cb08e1e0bdb7b35404e7de9efd48099dd731484
markn <markn@aesclever.com>
2022-01-02 23:32:59 -0800

    Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.

--- codechallenge_102.py
+++ codechallenge_102.py
@@ -34,115 +34,113 @@ Note: (list splicing)
 '''
 import unittest
 
-dictEncodedMsg = {'1':'a', '2':'b', '3':'c', '4':'d', '5':'e', '6':'f', 
-                   '7':'g', '8':'h', '9':'i', '10':'j', '11':'k', '12':'l', 
-                   '13':'m', '14':'n', '15':'o', '16':'p', '17':'q', '18':'r', 
-                   '19':'s', '20':'t', '21':'u', '22':'v', '23':'w', '24':'x', '25':'y', '26':'z'}
-
+class Enigma:
+    def __init__(self, message, dictEncodedMsg):
+        self.message = message
+        self.dictEncodedMsg = dictEncodedMsg
     
-def encodedValue(key):
-    if key in dictEncodedMsg:
-        return dictEncodedMsg[key]
-    else:
-        return ''
+    @property
+    def encodedValue(self):
+        return self.dictEncodedMsg
+    
+    def encodedValue(self, key):
+        if key in self.dictEncodedMsg:
+            return self.dictEncodedMsg[key]
+        else:
+            return ''
                 
     
-    
-def addElement(result_lst, tmp_lst):
-    #print (tmp_lst)
-    [i for i in tmp_lst if i != '']
+    def addElement(self, result_lst, tmp_lst):
+        #print (tmp_lst)
+        [i for i in tmp_lst if i != '']
 
-    if len(tmp_lst) > 0: result_lst.append(''.join(tmp_lst))
-    #print (result_lst)
-    return result_lst
-    
-def countDecoding(message):
-    # remove leading zeros
-    message = message.lstrip('0')   
-    
-    tmp_lst = []
-    result_lst = []
-    
-	# edge case:
-    if len(message) == 0:
-        return 0
-
-    # slpit message string into list of charaters
-    msgKey = list(message)
- 
-    #
-    # Base case: each element is individually matched with the dictionary
-    #
-    [tmp_lst.append(encodedValue(''.join(msgKey[idx]))) for idx in range(0, len(msgKey))]
-    result_lst.append(''.join(tmp_lst))
-    
-    
-    #
-    # Iterable case: 1st element is matched with the dictionary, 
-    # 2nd concat 3rd element is matched with the dictionary, 
-    # then 3rd element is matched with the dictionary,...
-    #
-    # Permutate the list of characters.    
-    for i in range(0, len(msgKey)):
-        tmp_lst = []
-        [tmp_lst.append(encodedValue(''.join(msgKey[:idx]))) for idx in range(i, len(msgKey))]
-        result_lst.append(''.join(tmp_lst))
-
-        tmp_lst = []
-        [tmp_lst.append(encodedValue(''.join(msgKey[idx:]))) for idx in range(i, len(msgKey))]
-        result_lst.append(''.join(tmp_lst))
-
-    tmp_lst = []
-    
-    # Flip the list of characters in msgKey
-    msgKey = msgKey[::-1]
-    for i in range(0, len(msgKey)):
-        tmp_lst = []
-        [tmp_lst.append(encodedValue(''.join(msgKey[:idx]))) for idx in range(i, len(msgKey))]
-        result_lst.append(''.join(tmp_lst))
-
-        tmp_lst = []
-        [tmp_lst.append(encodedValue(''.join(msgKey[idx:]))) for idx in range(i, len(msgKey))]
-        result_lst.append(''.join(tmp_lst)) 
-
-
+        if len(tmp_lst) > 0: result_lst.append(''.join(tmp_lst))
+        #print (result_lst)
+        return result_lst
         
+    def countDecoding(self):
+        # remove leading zeros
+        self.message = self.message.lstrip('0')   
+        
+        tmp_lst = []
+        result_lst = []
+        
+        # edge case:
+        if len(self.message) == 0:
+            raise ValueError("Empty message")
+            
 
-    # remove duplicates
-    result_lst = list(set(result_lst))
+        # slpit message string into list of charaters
+        msgKey = list(self.message)
     
-    # remove elements with length 1
-    final_result = [i for i in result_lst if len(i) >= 2]    
-    
-    print(final_result)
-    return len(final_result)
+        #
+        # Base case: each element is individually matched with the dictionary
+        #
+        [tmp_lst.append(self.encodedValue(''.join(msgKey[idx]))) for idx in range(0, len(msgKey))]
+        result_lst.append(''.join(tmp_lst))
+        
+        
+        #
+        # Iterable case: 1st element is matched with the dictionary, 
+        # 2nd concat 3rd element is matched with the dictionary, 
+        # then 3rd element is matched with the dictionary,...
+        #
+        # Permutate the list of characters.    
+        for i in range(0, len(msgKey)):
+            tmp_lst = []
+            [tmp_lst.append(self.encodedValue(''.join(msgKey[:idx]))) for idx in range(i, len(msgKey))]
+            result_lst.append(''.join(tmp_lst))
+
+            tmp_lst = []
+            [tmp_lst.append(self.encodedValue(''.join(msgKey[idx:]))) for idx in range(i, len(msgKey))]
+            result_lst.append(''.join(tmp_lst))
+
+        tmp_lst = []
+        
+        # Flip the list of characters in msgKey
+        msgKey = msgKey[::-1]
+        for i in range(0, len(msgKey)):
+            tmp_lst = []
+            [tmp_lst.append(self.encodedValue(''.join(msgKey[:idx]))) for idx in range(i, len(msgKey))]
+            result_lst.append(''.join(tmp_lst))
+
+            tmp_lst = []
+            [tmp_lst.append(self.encodedValue(''.join(msgKey[idx:]))) for idx in range(i, len(msgKey))]
+            result_lst.append(''.join(tmp_lst)) 
+
+
+            
+
+        # remove duplicates
+        result_lst = list(set(result_lst))
+        
+        # remove elements with length 1
+        final_result = [i for i in result_lst if len(i) >= 2]    
+        
+        print(final_result)
+        return len(final_result)
 
 class TestDecoder(unittest.TestCase):
     def test_decode(self):
-        self.assertEqual(countDecoding('111'), 3)
-        self.assertEqual(countDecoding('12'), 3)
-        self.assertEqual(countDecoding('1234'), 5)
-        
+        mesg = '111'
+        encodedMsg = {'1':'a', '2':'b', '3':'c', '4':'d', '5':'e', '6':'f', '7':'g', '8':'h', '9':'i', '10':'j', '11':'k', '12':'l', '13':'m', '14':'n', '15':'o', '16':'p', '17':'q', '18':'r', '19':'s', '20':'t', '21':'u', '22':'v', '23':'w', '24':'x', '25':'y', '26':'z'}
+        enigma = Enigma(mesg, encodedMsg)
+   
+        self.assertEqual(enigma.countDecoding(), 3)
+        enigma = Enigma('1211', encodedMsg)
+        self.assertEqual(enigma.countDecoding(), 5)
+        enigma = Enigma('1234', encodedMsg)
+        self.assertEqual(enigma.countDecoding(), 3)
+        enigma = Enigma('asasdggsad', encodedMsg)
+        with self.assertRaises(ValueError):
+            enigma.countDecoding()
     
 if __name__ == '__main__':
-    print(countDecoding('111'))  
-    print(countDecoding('12')) 
-    print(countDecoding('1234')) 
-    print(countDecoding('12341234')) 
-    print(countDecoding('123412341234')) 
-    print(countDecoding('1234123412341234')) 
-    print(countDecoding('123412341234123412341234')) 
-    print(countDecoding('12341234123412341234123412341234')) 
-    print(countDecoding('1234123412341234123412341234123412341234')) 
-    print(countDecoding('123412341234123412341234123412341234123412341234')) 
-    print(countDecoding('1234123412341234123412341234123412341234123412341234')) 
-    print(countDecoding('12341234123412341234123412341234123412341234123412341234')) 
-    print(countDecoding('123412341234123412341234123412341234123412341234123412341234')) 
-    print(countDecoding('1234123412341234123412341234123412341234123412341234123412341234')) 
-    print(countDecoding('12341234123412341234123412341234123412341234123412341234123412341234')) 
-    print(countDecoding('123412341234123412341234123412341234123412341234123412341234123412341234')) 
-    print(countDecoding('1234123412341234123412341234123412341234123412341234123412341234123412341234')) 
-    print(countDecoding('1234123412341234123412'))
+    mesg = '111'
+    encodedMsg = {'1':'a', '2':'b', '3':'c', '4':'d', '5':'e', '6':'f', '7':'g', '8':'h', '9':'i', '10':'j', '11':'k', '12':'l', '13':'m', '14':'n', '15':'o', '16':'p', '17':'q', '18':'r', '19':'s', '20':'t', '21':'u', '22':'v', '23':'w', '24':'x', '25':'y', '26':'z'}
+    enigma = Enigma(mesg, encodedMsg)
+    print(enigma.countDecoding())
+    
           
     unittest.main()
     
