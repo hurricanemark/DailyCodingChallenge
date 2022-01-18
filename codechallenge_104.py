1d643e748169813874c0112ef33ce17e974d19cd
markn <markn@aesclever.com>
2022-01-17 21:19:01 -0800

    Given a string representing the file system in the above format, return the length of the longest absolute path to a file in the abstracted file system.

--- codechallenge_104.py
+++ codechallenge_104.py
@@ -17,13 +17,15 @@ The string "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsub
 
 `print("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext")`
 
-dir
-    subdir1
-        file1.ext
-        subsubdir1
-    subdir2
-        subsubdir2
-            file2.ext
+Level   Name
+-----   ----
+0       dir
+1           subdir1
+2               file1.ext           <-- this qualifies as an absolute path to a file
+2               subsubdir1          <-- this qualifies as a path to a folder
+1           subdir2
+2               subsubdir2
+3                   file2.ext       <-- this qualifies as a path
             
 The directory dir contains two sub-directories subdir1 and subdir2. subdir1 contains a file file1.ext and an empty second-level sub-directory subsubdir1. subdir2 contains a second-level sub-directory subsubdir2 containing a file file2.ext.
 We are interested in finding the longest (number of characters) absolute path to a file within our file system. For example, in the second example above, the longest absolute path is "dir/subdir2/subsubdir2/file2.ext", and its length is 32 (not including the double quotes).
@@ -31,71 +33,106 @@ Given a string representing the file system in the above format, return the leng
 
 Note:
 =====
-The name of a file contains at least a period and an extension.
-The name of a directory or sub-directory will not contain a period.
+The name of a file contains at least a period and an extension.  Hmmm, what about a file with no extension?
+The name of a directory or sub-directory will not contain a period.  Hmmm, what about a directory with periods in its name?
+
 
 Algorithm:
 ==========
 1.  Validate input
-2.  Tokenize the input string
-3.  Concatenate the tokens into a list of strings, each string representing an absolute path will end with at least a period and an extension.
-4.  Compare the length of each string to the longest length found so far.
+2.  Tokenize the input string where the delimeter is '\n'.  Determine directory level by equating the number of leading tabs.
+3.  Strip the leading tabs and spaces from each token, append a '/' if it is a directory
+4.  Concatenate the tokens into a list of strings, each string representing an absolute path will end with at least a period and an extension.
+5.  Compare the length of each string to the longest length found so far.
 '''
+
 import re as regexp
+import unittest
+import HtmlTestRunner
+import os
+
+#
+# Bruteforce approach
+#
 def longest_absolute_path(input_string):
+    
+    # 1.  Validate input
     if not input_string:
         raise Exception("Empty input string")
-    
-    # tokenize the input string
+
+    # 2.  tokenize the input string
     tokens = input_string.split('\n')
-    
-    # concatenate the tokens into a list of strings, each string representing an absolute path will end with at least a period and an extension.
-    paths = []
+
+    # 3.  Determine directory level by number of leading tabs
+    level = 0    
+    qualified_paths = []
+    prev_is_a_file = False
+    tcount = 0
+    longest_path = []
+    print("TabCnt\tLevel\tisFile\tToken")
+    print("=======\t=====\t=====\t=====")
     for token in tokens:
-        if token.endswith('.'):
-            paths.append(token)
-        else:
-            paths.append(token)
-
-    arr = ['dir', '\tsubdir1', '\t\tfile1.ext']
-    print(count_tabs(arr))
-    
-    tmpPaths = []
-    [print(x) for x in paths if x.startswith('\t')]
-    print('-----------------')
-    [ tmpPaths.append(regexp.sub(r"[\n\t\s]*", "", x)) for x in paths] # remove leading tabs    
-    print(tmpPaths)
-    
-    # compare the length of each string to the longest length found so far.
-    longest_length = 0
-    for path in paths:
-        if len(path) > longest_length:
-            longest_length = len(path)
+        if token.count('\t') == 0:  # root directory
+            longest_path.append(token)
+            longest_path.append('/')
+            level = 0
+            prev_is_a_file = False
             
+            #print("TabCnt: {} token: {}".format(tcount, token))
+        elif token.count('\t') > 0:
+            tcount = token.count('\t', 0, len(token))
+            
+            if tcount > level:
+                # is it a file or a directory?
+                if token.count('.', 0, len(token)) == 0:
+                    # a directory
+                    longest_path.append(regexp.sub(r"[\n\t\s]*", "", token))
+                    longest_path.append('/')
+                    qualified_paths.append(''.join(longest_path))   # path could end in just a folder
+                    prev_is_a_file = False
+                else:
+                    # a file
+                    longest_path.append(regexp.sub(r"[\n\t\s]*", "", token))
+                    qualified_paths.append(''.join(longest_path))   # path could end in just a file
+                    longest_path = longest_path[:-1]
+                    prev_is_a_file = True
+                
+            elif tcount == level:
+                longest_path.append(regexp.sub(r"[\n\t\s]*", "", token))
+                longest_path.append('/')
+                prev_is_a_file = False
+                
+            else: # tcout <= level
+                #print("Back level detected: tcount:{} level:{}".format(tcount, level))
+                longest_path = longest_path[0:(tcount + 1)]
+                longest_path.append(regexp.sub(r"[\n\t\s]*", "", token))
+                longest_path.append('/')
+                
+            level = tcount
+            
+        print("{}\t{}\t{}\t{}".format(tcount, level, prev_is_a_file, token))    
+
     
-    return longest_length
-
-
-
-def count_tabs(arr):
-    count_tab=0
-    count_space=0
-    count_newline=0
-    if type(arr) == list:
-        print(arr)
-        for x in arr:
-            if x.startswith('\t'):
-                x.count('\t', 0, len(x)):
-                    count_tab += x.count('\t', 0, len(x))
-            elif x.startswith(' '):
-                count_space += 1
-            elif x.startswith('\n'):
-                count_newline += 1
-
-    print("How many Tabs are present in the array? ", count_tab)
-    print("How many Spaces are present in the array? ", count_space)
-    print("How many Newlines are present in the array? ", count_newline)
-
+    if __debug__:  # debug seems to be always ON
+        # returning the number of characters in the longest path, and the longest path itself
+        return len(max(qualified_paths, key=len)), max(qualified_paths, key=len)
+    else:
+        # command syntax: `pipenv run python -O codechallenge_104.py`
+        return len(max(qualified_paths, key=len))
+    
+class TestLongestAbsolutePath(unittest.TestCase):
+    def test_absolute_path_len(self):
+        self.assertEqual(longest_absolute_path("dir\n\tsubdir1\n\t\tsubsubdir2\n\t\t\tfile.ext"), (31, 'dir/subdir1/subsubdir2/file.ext'))  
+        self.assertEqual(longest_absolute_path("PythonDevSrc\n\tProjects\n\t\tCodingChallenges\n\t\t\tcodechallenge_104.py"), (59, 'PythonDevSrc/Projects/CodingChallenges/codechallenge_104.py'))
+        
+        
 if __name__ == '__main__':
-    print(longest_absolute_path("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"))
+    if os.environ.get('UNITTEST_ONLY') != 'True':
+        print(longest_absolute_path("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"))
+        print()
+        print(longest_absolute_path("PythonDevSrc\n\tProjects\n\t\tCodingChallenges\n\t\t\tcodechallenge_104.py"))
+        print()
+        print(longest_absolute_path("dir\n\tsubdir1\n\t\tsubsubdir2\n\t\t\tfile.ext"))
+    else:    
+        unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='test_reports'))
     
