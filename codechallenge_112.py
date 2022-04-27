a670471e51de2c18dad4701c6778531ebcda1f92
markn <markn@aesclever.com>
2022-04-27 00:10:23 -0700

    Base on the output of observation 1, we can approach the sentence from both ends, defining the offset for blanks as {[0,5],[1,3],[2,1,2],[3,2]}.

--- codechallenge_112.py
+++ codechallenge_112.py
@@ -18,68 +18,9 @@ Observation 1:
 =============
 Number of columns = len(sentence)
 Number of rows = k
-X=[cols][rows]
+X=[rows][cols]
 
-X[0][0] = sentence[0]
-X[0][1] = ' '
-X[0][2] = ' '
-X[0][3] = ' '
-X[0][4] = ' '
-X[0][5] = ' '
-X[0][6] = sentence[6]
-X[0][7] = ' '
-X[0][8] = ' '
-X[0][9] = ' '
-X[0][10] = ' '
-X[0][11] = ' '
-X[0][12] = sentence[13]
-
-X[1][0] = ' '
-X[1][1] = sentence[1]
-X[1][2] = ' '
-X[1][3] = ' '
-X[1][4] = ' '
-X[1][5] = sentence[5]
-X[1][6] = ' '
-X[1][7] = sentence[7]
-X[1][8] = ' '
-X[1][9] = ' '
-X[1][10] = ' '
-X[1][11] = sentence[11]
-X[1][12] = ' '
-
-X[2][0] = ' '
-X[2][1] = ' '
-X[2][2] = sentence[2]
-X[2][3] = ' '
-X[2][4] = sentence[4]
-X[2][5] = ' '
-X[2][6] = ' '
-X[2][7] = ' '
-X[2][8] = sentence[8]
-X[2][9] = ' '
-X[2][10] = sentence[10]
-X[2][11] = ' '
-X[2][12] = ' '
-
-X[3][0] = ' '
-X[3][1] = ' '
-X[3][2] = ' '
-X[3][3] = sentence[3]
-X[3][4] = ''
-X[3][5] = ' '
-X[3][6] = ' '
-X[3][7] = ' '
-X[3][8] = ' '
-X[3][9] = sentence[9]
-X[3][10] = ' '
-X[3][11] = ' '
-X[3][12] = ' '
-
-for r in X:
-   print(r)
-
-# Output:
+Let's bruteforce by filling out the 2d array to print the desired zigzag
 ['t', ' ', ' ', ' ', ' ', ' ', 'a', ' ', ' ', ' ', ' ', ' ', 'g']
 [' ', 'h', ' ', ' ', ' ', 's', ' ', 'z', ' ', ' ', ' ', 'a', ' ']
 [' ', ' ', 'i', ' ', 'i', ' ', ' ', ' ', 'i', ' ', 'z', ' ', ' ']
@@ -172,7 +113,9 @@ def bruteforce(sentence, k):
     # iterate through rows
     for r in X:
         print(r)
-        
+      
+      
+# Use numpy.fill_diagonal         
 def smarterAlgo(sentense, k):
     rows = k
     cols = len(sentence)
