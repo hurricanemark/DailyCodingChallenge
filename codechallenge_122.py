a01966855a559082b500e9b53599a860f8511b3e
markn <markn@aesclever.com>
2022-06-30 01:18:40 -0700

    add chromedriver as webdriver

--- codechallenge_122.py
+++ codechallenge_122.py
@@ -9,8 +9,19 @@ Controlling the browser with the Selenium module.
 '''
 from selenium import webdriver
 
-browser = webdriver.Firefox()
+browser = webdriver.Chrome()
+# browser = webdriver.Firefox()
 type(browser)
 
 
-browser.get('https://www.aesclever.com/')
+browser.get('https://github.com/hurricanemark/')
+
+try:
+    elem = browser.find_elements_by_link_text('DailyCodingChallenge')
+    type(elem)
+    elem.click()
+    print('Found <%s> element with the text "DailyCodingChallenge"' % (elem.tag_name))
+except Exception as e:
+    print(e)
+    browser.quit()
+    exit()
